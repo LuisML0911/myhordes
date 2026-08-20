4a5e2286622c5b02e6a53052def5113a74b50a93
LuisML0911 <lamll0911@gmail.com>
2026-08-20 14:36:54 -0600

    Update automatic.spec.js

--- tests/automatic.spec.js
+++ tests/automatic.spec.js
@@ -1517,28 +1517,8 @@ async function getGestHordes(){
 	return await response.json();
 }
 async function getWeaponsGestHordes() {
-	const baseDir = process.env.LOCALAPPDATA || process.env.HOME;
-	let browserTemp = await chromium.launchPersistentContext(path.join(baseDir, 'playwright-profiles', randomUUID()), {
-		channel: 'msedge',
-		headless: true
-	});
-	let pagTemp = await browserTemp.newPage();
-	const response = await pagTemp.request.get(`https://gesthordes.fr/rest/v1/prototype/all`, {
-		headers: {
-			"accept": "application/json",
-			"accept-encoding": "gzip, deflate, br, zstd",
-			"accept-language": "es-419,es;q=0.9,es-ES;q=0.8,en;q=0.7,en-GB;q=0.6,en-US;q=0.5",
-			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36 Edg/151.0.0.0",
-			"dnt": "1",
-			"upgrade-insecure-requests": "1"
-		}
-	});
-	if (!response.ok()) {
-		throw new Error("Error en la petición: " + response.status());
-	}
-	const result = await response.json();
-	await browserTemp.close();
-	return result;
+  const raw = await fs.readFile('./data/weapons.json', 'utf-8');
+  return JSON.parse(raw);
 }
 
 // actualiza GestHordes
