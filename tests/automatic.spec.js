99631989b80f098b3bb5c5b824f4dfd92216d45f
LuisML0911 <lamll0911@gmail.com>
2026-08-21 00:09:24 -0600

    Refactor fetch call in automatic.spec.js

--- tests/automatic.spec.js
+++ tests/automatic.spec.js
@@ -546,19 +546,17 @@ test('GetGestHordes', async ({page}) => {
 	await page.goto('https://gesthordes.fr/news', { waitUntil: 'networkidle' });	
 	const cookies = await page.context().cookies();
 	console.log(cookies);
-
 	const response = await page.evaluate(async (idTown) => {
-		console.log(`https://gesthordes.fr/rest/v1/carte/${idTown}`);
-	const res = await fetch(`https://gesthordes.fr/rest/v1/carte/${idTown}`, {
-		headers: {
-			"accept": "application/json",
-			"gh-mapid": parseInt(idTown, 10)
-		}
-	});
-	return res.json();
+		const res = await fetch(`https://gesthordes.fr/rest/v1/carte/${idTown}`, {
+			headers: {
+				"accept": "application/json",
+				"gh-mapid": parseInt(idTown, 10)
+			}
+		});
+		return res.json();
 	}, idTown);
 	console.log(response);
-	await page.goto(`https://gesthordes.fr/carte/${idTown}`);
+	//await page.goto(`https://gesthordes.fr/carte/${idTown}`);
 
 	writeJSON('data/gestHordes.json', response);
 });
