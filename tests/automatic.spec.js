f40b9ba8515f288974ae07acc3880b168a91d588
LuisML0911 <lamll0911@gmail.com>
2026-08-23 03:19:23 -0600

    Reduce test timeout from 320s to 150s

--- tests/automatic.spec.js
+++ tests/automatic.spec.js
@@ -562,7 +562,7 @@ test('GetGestHordes', async ({page}) => {
 });
 
 test('myhordes', async () => { 
-	test.setTimeout(320000);
+	test.setTimeout(150000);
 	//fs.rmSync(path.join(process.env.LOCALAPPDATA, 'playwright-profiles'), { recursive: true, force: true });
 	const baseDir = process.env.LOCALAPPDATA || process.env.HOME;
 	uniqueProfile = path.join(baseDir, 'playwright-profiles', randomUUID());
